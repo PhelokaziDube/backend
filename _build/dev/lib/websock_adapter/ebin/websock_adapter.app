d5574ba3b76f1b727d06f681773fdc7444b4b495
Yusuf Motara <y.motara@ru.ac.za>
2023-10-02 14:21:05 +0200

    Stop storing development artifacts

--- _build/dev/lib/websock_adapter/ebin/websock_adapter.app
+++
@@ -1,6 +0,0 @@
-{application,websock_adapter,
-             [{applications,[kernel,stdlib,elixir,websock,plug]},
-              {description,"A set of WebSock adapters for common web servers"},
-              {modules,['Elixir.WebSockAdapter']},
-              {registered,[]},
-              {vsn,"0.5.4"}]}.
--- deps/jiffy/c_src/jiffy.d
+++
@@ -1,5 +0,0 @@
-c_src/jiffy.o: c_src/jiffy.c c_src/jiffy.h \
- /nix/store/qaav7xj0rmcylz1n8n1xal06nk2xs0p8-erlang-25.3.2.6/lib/erlang/erts-13.2.2.3/include/erl_nif.h \
- /nix/store/qaav7xj0rmcylz1n8n1xal06nk2xs0p8-erlang-25.3.2.6/lib/erlang/erts-13.2.2.3/include/erl_drv_nif.h \
- /nix/store/qaav7xj0rmcylz1n8n1xal06nk2xs0p8-erlang-25.3.2.6/lib/erlang/erts-13.2.2.3/include/erl_int_sizes_config.h \
- /nix/store/qaav7xj0rmcylz1n8n1xal06nk2xs0p8-erlang-25.3.2.6/lib/erlang/erts-13.2.2.3/include/erl_nif_api_funcs.h
